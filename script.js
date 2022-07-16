dbb5ff5d6aadca6143b957d2b69eba4742d8a8e1
noceive <nocivescience@gmail.com>
2022-07-16 02:56:10 -0400

    se mueve todo el tren

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const containerElement = document.querySelector('.container');
+const containerEl = document.querySelector('.container');
 const cardsEl= document.querySelectorAll('.card');
 const cardsArray = [...cardsEl];
 const prevEl = document.querySelector('.prev');
 const nextEl = document.querySelector('.next');
+let contador = 0;
+function sliders(){
+    containerEl.style.transform = `translateX(${-90}%)`;
+    containerEl.style.transition = 'transform 0.5s';
+    if(contador == cardsArray.length){
+        containerEl.style.transform = `translateX(${0}%)`;
+        containerEl.style.transition = 'transform 0.5s';
+        contador = 1;
+    }
+};
+setInterval(()=>{
+    sliders();
+    contador++;
+}, 1000);
