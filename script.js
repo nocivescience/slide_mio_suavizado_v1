aab34444df3a53de56cf490e5083f5afd9936fb4
noceive <nocivescience@gmail.com>
2022-07-16 02:30:54 -0400

    mi primer commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const containerElement = document.querySelector('.container');
+const cardsEl= document.querySelectorAll('.card');
+const cardsArray = [...cardsEl];
+const prevEl = document.querySelector('.prev');
+const nextEl = document.querySelector('.next');
